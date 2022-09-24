0773c630c630c5f88b777398ce48c701dd79b237
Kaden Taylor <a02257212@aggies.usu.edu>
2022-09-24 13:05:10 -0600

    Implemented secant.py

--- hw2/secant.py
+++ hw2/secant.py
@@ -1,1 +1,15 @@
- 
+import math as math
+
+
+def secant(f, x0, x1, tol):
+    print("newton iteration")
+    if abs(f(x0)) < tol:
+        return x0
+    else:
+        return secant(f, x1, x0 - ((x1-x0) * f(x0)) / (f(x1) - f(x0)), tol)
+
+
+f = lambda x: x**2 - 3
+
+print(f"Newton Method: {secant(f, 1.7, 1.6, .00000001)}")
+

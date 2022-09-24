57831425d2326889f282240e20118aeac98b7c19
Kaden Taylor <a02257212@aggies.usu.edu>
2022-09-24 12:55:59 -0600

    Implemented newton method and created a secant.py

--- hw2/newton.py
+++ hw2/newton.py
@@ -1,1 +1,16 @@
- 
+import math as math
+
+
+def newton(f, df, x0, tol):
+    print("newton iteration")
+    if abs(f(x0)) < tol:
+        return x0
+    else:
+        return newton(f, df, x0 - f(x0)/df(x0), tol)
+
+
+f = lambda x: x*math.e**(-x)
+df = lambda x: math.e**(-x) - (x * math.e**(-x))
+
+print(f"Newton Method: {newton(f, df, .5, .00000001)}")
+

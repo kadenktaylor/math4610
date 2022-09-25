28a3ee0950b227417848660a8f072f173474291e
Kaden Taylor <a02257212@aggies.usu.edu>
2022-09-24 21:48:19 -0600

    Create docs to add to.

---
+++ hw2/hw2.md
@@ -0,0 +1,5 @@
+# Math 4610 Assignement 2
+
+## Task 1: Newton's Method for Root Finding.
+
+Here is a link to my 
--- hw2/hybrid_secant.py
+++ hw2/hybrid_secant.py
@@ -11,7 +11,6 @@ def hybrid_secant(f, a, b, tol, maxIter, v):
         x2 = x0 - ((x1-x0) * f(x0)) / (f(x1) - f(x0))
         secant_error = abs(x2 - x0)
         if secant_error > error:
-            print("Bisection Iteration")
             fa = f(a)
             fb = f(b)
             for i in range(1, 4):
@@ -27,7 +26,6 @@ def hybrid_secant(f, a, b, tol, maxIter, v):
             x0 = .49*(a+b)
             x1 = .51*(a+b)
         else:
-            print("Newton Iteration")
             x0 = x1
             x1 = x2
             error = secant_error
